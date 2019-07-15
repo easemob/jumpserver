fc82e9b203b3aca28e696a26685c43a9b4687a5a
单靖恒 <1198955192@qq.com>
2019-07-15 12:04:43 +0800

    Revert "Easemob 1.5.1"

--- apps/alicloud/models/asset.py
+++ apps/alicloud/models/asset.py
@@ -5,7 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class Asset(models.Model):
     id = models.UUIDField(primary_key=True)
-    node_id = models.UUIDField()
     type = models.CharField(max_length=128, verbose_name=_('AssetType'))
     instance_id = models.CharField(max_length=128, verbose_name=_('InstanceId'))
     instance_name = models.CharField(max_length=128, verbose_name=_('InstanceName'))
