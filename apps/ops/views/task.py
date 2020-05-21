f51b443bfe04cfa1451877a53842de3cbab840ec
shanjingheng <shanjh@easemob.com>
2020-05-21 18:53:01 +0800

    add task update and delete function

--- apps/ops/views/task.py
+++ apps/ops/views/task.py
@@ -1,7 +1,9 @@
 # ~*~ coding: utf-8 ~*~
-
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils.translation import ugettext as _
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views import View
+from django.views.generic import TemplateView, DetailView
 from common.mixins import DatetimeSearchMixin
 from common.permissions import PermissionsMixin, IsOrgAdmin
 from ..models import Task, TaskMeta
@@ -9,7 +11,8 @@ from ..models import Task, TaskMeta
 __all__ = [
     'TaskManagementListView',
     'TaskDetailInfoView',
-    'TaskExecutionHistoryView'
+    'TaskExecutionHistoryView',
+    'TaskUpdateView'
 ]
 
 
@@ -56,3 +59,13 @@ class TaskExecutionHistoryView(PermissionsMixin, DetailView):
         }
         kwargs.update(context)
         return super().get_context_data(**kwargs)
+
+
+class TaskUpdateView(PermissionsMixin, View):
+    permission_classes = [IsOrgAdmin]
+
+    def get(self, request, **kwargs):
+        task_meta = TaskMeta.objects.get(pk=kwargs.pop('pk'))
+        task_type = task_meta.task_type
+        if task_type == 'file_deploy':
+            return redirect(reverse('ops:file-task-update', kwargs={'pk': str(task_meta.id)}))
